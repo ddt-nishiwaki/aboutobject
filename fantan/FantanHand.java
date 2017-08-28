4017806961454f71cad195cd7f1abf3f2e8457cf
Seiji Nishiwaki <ddt.jissyusei2@gmail.co>
2017-08-29 04:06:31 +0900

    演習7-2 100%

---
+++ fantan/FantanHand.java
@@ -0,0 +1,21 @@
+package fantan;
+
+import oldmaid.Hand;
+
+/**
+ * 7-2 ババ抜きプログラムで作成した手札クラスを継承して、七並べ用の手札クラス「FantanHand」を作成してください
+ * 各メソッドの実際の処理まで記述する必要はありません
+ * 
+ * 七並べ用の手札クラス
+ * ババ抜き用手札クラスを継承する
+ * @author wakkky05
+ *
+ */
+public class FantanHand extends Hand{
+	/**
+	 * カードを見る
+	 */
+	public void lookCard(){
+		// 実際の処理
+	}
+}
